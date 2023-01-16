24f77bdcc5e8350b057355569d5f3d6426454b5f
Oscar Bernal <odbg2013@gmail.com>
2023-01-15 19:35:00 -0500

    add first title

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import '../assets/css/header.css'
+
+export default function Header() {
+	return (
+		<header>
+			<ul className='header-list'>
+				<li>Home</li>
+				<li>About</li>
+				<li>Tech Stack</li>
+				<li>Projects</li>
+				<li>Contact</li>
+			</ul>
+		</header>
+	)
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import Header from './Header.jsx'
+
+export default function Home() {
+	return (
+		<>
+			<Header />
+			<main>
+				<h1>Hi 👋, My name is Oscar Bernal I build things for web</h1>
+			</main>
+		</>
+	)
+}
