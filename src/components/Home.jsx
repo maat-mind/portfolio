f6337ce75fded2c08f1f901c0e805d3d3fe3f50e
Oscar Bernal <odbg2013@gmail.com>
2023-01-16 19:32:34 -0500

    add hide header items on md screen

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,14 @@
 import Header from './Header.jsx'
+import '../assets/css/home.css'
 
 export default function Home() {
 	return (
 		<>
 			<Header />
 			<main>
-				<h1>Hi 👋, My name is Oscar Bernal I build things for web</h1>
+				<section>
+					<h1>My name is Oscar Bernal and I build things for web</h1>
+				</section>
 			</main>
 		</>
 	)
