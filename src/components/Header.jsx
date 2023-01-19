700b51deb9a274a07dc6c4b9f5b2646910013be0
Oscar Bernal <odbg2013@gmail.com>
2023-01-19 18:42:13 -0500

    add sections

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,24 +11,42 @@ export default function Header() {
 			</div>
 			<div className='flex w-2/3'>
 				<section className='w-5/6'>
-					<ul className='flex justify-evenly text-2xl mx-4'>
-						<li className='mx-2'>Home</li>
-						<li className='mx-2'>About</li>
-						<li className='mx-2'>Tech Stack</li>
-						<li className='mx-2'>Projects</li>
-						<li className='mx-2'>Contact</li>
+					<ul className='flex justify-evenly text-2xl mx-4 text-gray-400'>
+						<li className='mx-2 hover:text-gray-100 cursor-pointer'>
+							<a href='#'>Home </a>
+						</li>
+						<li className='mx-2 hover:text-gray-100 cursor-pointer'>
+							<a href='#about'>About</a>
+						</li>
+						<li className='mx-2 hover:text-gray-100 cursor-pointer'>
+							<a href='#tech-stack'>Tech Stack</a>
+						</li>
+						<li className='mx-2 hover:text-gray-100 cursor-pointer'>
+							<a href='#projects'>Projects</a>
+						</li>
+						<li className='mx-2 hover:text-gray-100 cursor-pointer'>
+							<a href='#contact'>Contact</a>
+						</li>
 					</ul>
 				</section>
 				<section className='w-1/6'>
 					<ul className='flex justify-center'>
 						<li className='mx-4'>
 							<a href='https://github.com/maat-mind' target='_blank'>
-								<img src={githubIcon} alt='github logo icon' />
+								<img
+									className='hover:opacity-[0.8]'
+									src={githubIcon}
+									alt='github logo icon'
+								/>
 							</a>
 						</li>
 						<li className='mx-4'>
 							<a href='https://linkedin.com/in/maat-mind' target='_blank'>
-								<img src={linkedinIcon} alt='github logo icon' />
+								<img
+									className='hover:opacity-[0.8]'
+									src={linkedinIcon}
+									alt='github logo icon'
+								/>
 							</a>
 						</li>
 					</ul>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,14 +8,23 @@ export default function Home() {
 	return (
 		<>
 			<Header />
-			<main>
-				<section>
-					<h1 className=''>
-						My name is Oscar Bernal and I build things for web
-					</h1>
+			<main className='flex items-center mt-14 p-4'>
+				<section className='w-3/5'>
+					<span className='text-5xl'>
+						<h1>
+							Hi👋, <br />
+							My name is <br />
+						</h1>
+						<a className='font-bold bg-gradient-to-r from-cyan-500 to-indigo-400 inline-block text-transparent bg-clip-text'>
+							{' '}
+							Oscar Bernal
+						</a>{' '}
+						<br />
+						<h1>and I build things for web</h1>
+					</span>
 				</section>
-				<section>
-					<Canvas style={{ height: '500px' }}>
+				<section className='w-2/3'>
+					<Canvas className='transform-gpu' style={{ height: '500px' }}>
 						<OrbitControls enableZoom={false} />
 						<ambientLight intensity={0.5} />
 						<directionalLight position={[-2, 5, 2]} intensity={1} />
@@ -25,6 +34,18 @@ export default function Home() {
 					</Canvas>
 				</section>
 			</main>
+			<section id='about' className='text-3xl text-center font-bold'>
+				<h2>About</h2>
+			</section>
+			<section id='tech-stack' className='text-3xl text-center font-bold'>
+				<h2>Tech Stack</h2>
+			</section>
+			<section id='projects' className='text-3xl text-center font-bold'>
+				<h2>Projects</h2>
+			</section>
+			<section id='contact' className='text-3xl text-center font-bold'>
+				<h2>Contact</h2>
+			</section>
 		</>
 	)
 }
