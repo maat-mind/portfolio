ab240c2e974c00ffb3424cbe687fc1bf3ae60687
Oscar Bernal <odbg2013@gmail.com>
2023-01-22 16:41:08 -0500

    add responsive navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import githubIcon from '../assets/github.svg'
 import linkedinIcon from '../assets/linkedin.svg'
 import mailIcon from '../assets/mail.svg'
 import Typewriter from './Typewriter'
+import './styles.css'
 
 export default function Header() {
-	const [isNavOpen, setIsNavOpen] = useState(false)
-	return (
+	/* 	return (
 		<nav className='container flex items-center justify-around px-4 py-8 mx-auto'>
 			<div className='w-1/3'>
 				<a className='text-4xl'>Oscar </a>
@@ -68,6 +68,105 @@ export default function Header() {
 				</svg>
 			</div>
 		</nav>
+	) */
+
+	const [isNavOpen, setIsNavOpen] = useState(false)
+
+	return (
+		<header className='flex items-center justify-between border-b border-gray-400 py-8 px-8 md:px-2'>
+			<div className='w-1/3'>
+				<a className='text-4xl'>Oscar </a>
+				<a className='text-4xl font-bold'>Bernal</a>
+			</div>
+			<nav className='w-1/3'>
+				<section className='MOBILE-MENU flex xl:hidden justify-end'>
+					<div
+						className='HAMBURGER-ICON space-y-2'
+						onClick={() => setIsNavOpen((prev) => !prev)}
+					>
+						<span className='block h-0.5 w-8 animate-pulse bg-gray-600'></span>
+						<span className='block h-0.5 w-8 animate-pulse bg-gray-600'></span>
+						<span className='block h-0.5 w-8 animate-pulse bg-gray-600'></span>
+					</div>
+
+					<div className={isNavOpen ? 'showMenuNav' : 'hideMenuNav'}>
+						<div
+							className='absolute top-0 right-0 px-8 py-8'
+							onClick={() => setIsNavOpen(false)}
+						>
+							<svg
+								className='h-8 w-8 text-gray-600'
+								viewBox='0 0 24 24'
+								fill='none'
+								stroke='currentColor'
+								strokeWidth='2'
+								strokeLinecap='round'
+								strokeLinejoin='round'
+							>
+								<line x1='18' y1='6' x2='6' y2='18' />
+								<line x1='6' y1='6' x2='18' y2='18' />
+							</svg>
+						</div>
+
+						<ul className='flex flex-col items-center justify-between min-h-[250px]'>
+							<li className='border-b border-gray-400 my-8 uppercase'>
+								<a href='/about'>About</a>
+							</li>
+							<li className='border-b border-gray-400 my-8 uppercase'>
+								<a href='/portfolio'>Tech Stack</a>
+							</li>
+							<li className='border-b border-gray-400 my-8 uppercase'>
+								<a href='/contact'>Project</a>
+							</li>
+							<li className='border-b border-gray-400 my-8 uppercase'>
+								<a href='/contact'>Contact</a>
+							</li>
+						</ul>
+					</div>
+				</section>
+
+				<div className='hidden justify-start items-center space-x-14 xl:flex'>
+					<a href='#'>About</a>
+					<a href='#'>Tech Stack</a>
+					<a href='#'>Projects</a>
+					<a href='#'>Contact</a>
+				</div>
+			</nav>
+
+			<section className='w-1/3 hidden space-x-12 xl:flex justify-center'>
+				<a
+					className='flex items-center'
+					href='https://github.com/maat-mind'
+					target='_blank'
+				>
+					<img
+						className='w-7 hover:opacity-[0.8]'
+						src={githubIcon}
+						alt='github logo icon'
+					/>
+					<h3 className='hidden 2xl:inline 2xl:mx-6'>GitHub</h3>
+				</a>
+				<a
+					className='flex items-center'
+					href='https://linkedin.com/in/maat-mind'
+					target='_blank'
+				>
+					<img
+						className='w-7 hover:opacity-[0.8]'
+						src={linkedinIcon}
+						alt='github logo icon'
+					/>
+					<h3 className='hidden 2xl:inline 2xl:mx-6'>LinkedIn</h3>
+				</a>
+				<a href='#'>
+					<img
+						className='w-7 hover:opacity-[0.8]'
+						src={mailIcon}
+						alt='email icon'
+					/>
+				</a>
+			</section>
+		</header>
 	)
 }
 
