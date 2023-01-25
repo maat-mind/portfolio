f5bf9a90ca0ace356c515c85412a77c31430754e
Oscar Bernal <odbg2013@gmail.com>
2023-01-24 20:20:36 -0500

    add projects info 1/2

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,76 +1,141 @@
 export default function Projects() {
 	return (
-		<div className='flex flex-wrap w-[auto] md:w-[auto] justify-center gap-4 mt-10'>
-			<div class='max-w-sm bg-white border border-gray-200 rounded-lg shadow'>
-				<a href='#'>
+		<div className='flex flex-wrap w-[auto] md:w-[1200px] m-auto justify-center gap-4 mt-10'>
+			<div class='flex flex-col gap-y-4 max-w-sm h-auto bg-white border border-gray-200 rounded-lg shadow py-4 px-[10px]'>
+				<h4 className='text-center font-bold text-2xl'>Donut.com</h4>
+				<p className='text-center'>
+					This is my first website using three.js following official
+					documentation and youtube tutorials
+				</p>
+				<video
+					src='https://user-images.githubusercontent.com/57721146/211682632-3d93afd8-888e-4e66-b1ba-d82bd9f844cb.mp4'
+					controls
+					loop
+					preload='auto'
+				>
+					Tu navegador no admite el elemento <code>video</code>.
+				</video>
+
+				<span className='flex h-full justify-evenly items-center mx-5 gap-2 max-w-sm flex-wrap'>
 					<img
-						class='rounded-t-lg'
-						src='/docs/images/blog/image-1.jpg'
-						alt=''
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg'
 					/>
-				</a>
-				<div class='p-5'>
-					<video
-						src='https://user-images.githubusercontent.com/57721146/211682632-3d93afd8-888e-4e66-b1ba-d82bd9f844cb.mp4'
-						controls
-						loop
-						preload='auto'
-					>
-						Tu navegador no admite el elemento <code>video</code>.
-					</video>
-				</div>
+					<img
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg'
+					/>
+					<img
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/threejs/threejs-original.svg'
+					/>
+				</span>
 			</div>
 
-			<div class='max-w-sm bg-white border border-gray-200 rounded-lg shadow'>
-				<a href='#'>
+			<div class='flex flex-col gap-y-4 max-w-sm h-auto bg-white border border-gray-200 rounded-lg shadow py-4 px-[10px]'>
+				<h4 className='text-center font-bold text-2xl'>Henry Dogs</h4>
+				<p className='text-center'>
+					The objective of this project was to reinforce and connect the
+					concepts learned in the bootcamp
+				</p>
+				<video
+					src='https://user-images.githubusercontent.com/57721146/204037737-bb346711-1dd8-4847-a832-b9a350d388f2.mp4'
+					controls
+					loop
+					preload='auto'
+				>
+					Tu navegador no admite el elemento <code>video</code>.
+				</video>
+
+				<span className='flex justify-evenly h-full items-center mx-5 gap-2 max-w-sm flex-wrap'>
 					<img
-						class='rounded-t-lg'
-						src='/docs/images/blog/image-1.jpg'
-						alt=''
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg'
 					/>
-				</a>
-				<div class='p-5'>
-					<video
-						src='https://user-images.githubusercontent.com/57721146/204037737-bb346711-1dd8-4847-a832-b9a350d388f2.mp4'
-						controls
-						loop
-						preload='auto'
-					>
-						Tu navegador no admite el elemento <code>video</code>.
-					</video>
-				</div>
+					<img
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg'
+					/>
+					<img
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg'
+					/>
+
+					<img
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg'
+					/>
+
+					<img
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg'
+					/>
+
+					<img
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg'
+					/>
+					<img
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg'
+					/>
+					<img
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg'
+					/>
+					<img
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg'
+					/>
+				</span>
 			</div>
 
-			<div class='max-w-sm bg-white border border-gray-200 rounded-lg shadow'>
-				<a href='#'>
+			<div class='flex flex-col gap-y-4 max-w-sm h-auto bg-white border border-gray-200 rounded-lg shadow py-4 px-[10px]'>
+				<h4 className='text-center font-bold text-2xl'>Crud Rest Api</h4>
+				<p className='text-center'>App built with Java Spring Boot Framework</p>
+
+				<img
+					className='object-contain max-h-[300px]'
+					src='https://user-images.githubusercontent.com/57721146/212571358-597a9b0d-eca7-4ddb-bfb1-7adf7170e728.png'
+					alt=''
+				/>
+
+				<span className='flex justify-evenly h-full items-center mx-5 gap-2 max-w-sm flex-wrap'>
 					<img
-						class='rounded-t-lg'
-						src='/docs/images/blog/image-1.jpg'
-						alt=''
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg'
 					/>
-				</a>
-				<div class='p-5'>
 					<img
-						src='https://user-images.githubusercontent.com/57721146/212571358-597a9b0d-eca7-4ddb-bfb1-7adf7170e728.png'
-						alt=''
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg'
 					/>
-				</div>
+				</span>
 			</div>
 
-			<div class='max-w-sm bg-white border border-gray-200 rounded-lg shadow'>
-				<a href='#'>
+			<div class='flex flex-col gap-y-4 max-w-sm h-auto bg-white border border-gray-200 rounded-lg shadow py-4 px-[10px]'>
+				<h4 className='text-center font-bold text-2xl'>Crud Rest Api</h4>
+				<p className='text-center'>
+					This social network is the final group project to graduate from
+					bootcamp, where I learned the tools and technologies needed to develop
+					a complete and functional web application.
+				</p>
+
+				<img
+					className='object-contain max-h-[300px]'
+					src='https://user-images.githubusercontent.com/57721146/214458490-998c0d27-1b92-4bb2-8904-722baeb66754.png'
+					alt=''
+				/>
+
+				<span className='flex justify-evenly h-full items-center mx-5 gap-2 max-w-sm flex-wrap'>
 					<img
-						class='rounded-t-lg'
-						src='/docs/images/blog/image-1.jpg'
-						alt=''
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg'
 					/>
-				</a>
-				<div class='p-5'>
 					<img
-						src='https://user-images.githubusercontent.com/57721146/214448828-b8b5df60-c8a3-484c-800c-c483a4b17c4d.png'
-						alt=''
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg'
 					/>
-				</div>
+				</span>
 			</div>
 
 			<div class='max-w-sm bg-white border border-gray-200 rounded-lg shadow'>
