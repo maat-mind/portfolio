f6ae5b367ea1901875cbb9965605abbab0a0c36b
Oscar Bernal <odbg2013@gmail.com>
2023-01-24 20:44:42 -0500

    add projects info 2/2

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,11 @@
 export default function Projects() {
 	return (
-		<div className='flex flex-wrap w-[auto] md:w-[1200px] m-auto justify-center gap-4 mt-10'>
-			<div class='flex flex-col gap-y-4 max-w-sm h-auto bg-white border border-gray-200 rounded-lg shadow py-4 px-[10px]'>
+		<div className='flex flex-wrap w-[auto] md:w-[90%] m-auto justify-center gap-4 mt-10'>
+			<div class='flex flex-col gap-y-4 max-w-sm w-[500px] min-w-[450px] h-auto bg-white border border-gray-200 rounded-lg shadow py-4 px-[10px]'>
 				<h4 className='text-center font-bold text-2xl'>Donut.com</h4>
 				<p className='text-center'>
 					This is my first website using three.js following official
-					documentation and youtube tutorials
+					documentation
 				</p>
 				<video
 					src='https://user-images.githubusercontent.com/57721146/211682632-3d93afd8-888e-4e66-b1ba-d82bd9f844cb.mp4'
@@ -32,7 +32,7 @@ export default function Projects() {
 				</span>
 			</div>
 
-			<div class='flex flex-col gap-y-4 max-w-sm h-auto bg-white border border-gray-200 rounded-lg shadow py-4 px-[10px]'>
+			<div class='flex flex-col gap-y-4 max-w-sm w-[500px] min-w-[450px] h-auto bg-white border border-gray-200 rounded-lg shadow py-4 px-[10px]'>
 				<h4 className='text-center font-bold text-2xl'>Henry Dogs</h4>
 				<p className='text-center'>
 					The objective of this project was to reinforce and connect the
@@ -55,10 +55,6 @@ export default function Projects() {
 					<img
 						className='w-12'
 						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg'
-					/>
-					<img
-						className='w-12'
-						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg'
 					/>
 
 					<img
@@ -83,14 +79,10 @@ export default function Projects() {
 						className='w-12'
 						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg'
 					/>
-					<img
-						className='w-12'
-						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg'
-					/>
 				</span>
 			</div>
 
-			<div class='flex flex-col gap-y-4 max-w-sm h-auto bg-white border border-gray-200 rounded-lg shadow py-4 px-[10px]'>
+			<div class='flex flex-col gap-y-4 max-w-sm w-[500px] min-w-[450px] h-auto bg-white border border-gray-200 rounded-lg shadow py-4 px-[10px]'>
 				<h4 className='text-center font-bold text-2xl'>Crud Rest Api</h4>
 				<p className='text-center'>App built with Java Spring Boot Framework</p>
 
@@ -112,8 +104,8 @@ export default function Projects() {
 				</span>
 			</div>
 
-			<div class='flex flex-col gap-y-4 max-w-sm h-auto bg-white border border-gray-200 rounded-lg shadow py-4 px-[10px]'>
-				<h4 className='text-center font-bold text-2xl'>Crud Rest Api</h4>
+			<div class='flex flex-col gap-y-4 max-w-sm w-[500px] min-w-[450px] h-auto bg-white border border-gray-200 rounded-lg shadow py-4 px-[10px]'>
+				<h4 className='text-center font-bold text-2xl'>Reply</h4>
 				<p className='text-center'>
 					This social network is the final group project to graduate from
 					bootcamp, where I learned the tools and technologies needed to develop
@@ -129,46 +121,98 @@ export default function Projects() {
 				<span className='flex justify-evenly h-full items-center mx-5 gap-2 max-w-sm flex-wrap'>
 					<img
 						className='w-12'
-						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg'
+					/>
+
+					<img
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg'
+					/>
+
+					<img
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg'
 					/>
 					<img
 						className='w-12'
-						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg'
+					/>
+					<img
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg'
+					/>
+					<img
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg'
+					/>
+					<img
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg'
 					/>
 				</span>
 			</div>
 
-			<div class='max-w-sm bg-white border border-gray-200 rounded-lg shadow'>
-				<a href='#'>
+			<div class='flex flex-col gap-y-4 max-w-sm w-[500px] min-w-[450px] h-auto bg-white border border-gray-200 rounded-lg shadow py-4 px-[10px]'>
+				<h4 className='text-center font-bold text-2xl'>React calculator</h4>
+				<p className='text-center'>
+					Interactive calculator that allows you to perform basic operations
+					with natural numbers
+				</p>
+
+				<img
+					className='object-contain max-h-[300px]'
+					src='https://user-images.githubusercontent.com/57721146/177380891-25e00068-216b-4feb-bc41-d1e71ad0f2db.gif'
+					alt=''
+				/>
+
+				<span className='flex justify-evenly h-full items-center mx-5 gap-2 max-w-sm flex-wrap'>
 					<img
-						class='rounded-t-lg'
-						src='/docs/images/blog/image-1.jpg'
-						alt=''
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg'
 					/>
-				</a>
-				<div class='p-5'>
-					<img src='https://user-images.githubusercontent.com/57721146/177380891-25e00068-216b-4feb-bc41-d1e71ad0f2db.gif' />
-				</div>
+					<img
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg'
+					/>
+					<img
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg'
+					/>
+				</span>
 			</div>
 
-			<div class='max-w-sm bg-white border border-gray-200 rounded-lg shadow'>
-				<a href='#'>
+			<div class='flex flex-col gap-y-4 max-w-sm w-[500px] min-w-[450px] h-auto bg-white border border-gray-200 rounded-lg shadow py-4 px-[10px]'>
+				<h4 className='text-center font-bold text-2xl w-[80%] m-auto'>
+					Redux toolkit & Tailwind counter
+				</h4>
+				<p className='text-center'>
+					Practice app using redux toolkit and tailwind css, simple counter app
+					that add, subtract and reset global state
+				</p>
+
+				<video
+					src='https://user-images.githubusercontent.com/57721146/205314772-57ba508c-22c7-4460-a021-e07e4852d20e.mp4'
+					controls
+					loop
+					preload='auto'
+				>
+					Tu navegador no admite el elemento <code>video</code>.
+				</video>
+
+				<span className='flex justify-evenly h-full items-center mx-5 gap-2 max-w-sm flex-wrap'>
 					<img
-						class='rounded-t-lg'
-						src='/docs/images/blog/image-1.jpg'
-						alt=''
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg'
 					/>
-				</a>
-				<div class='p-5'>
-					<video
-						src='https://user-images.githubusercontent.com/57721146/205314772-57ba508c-22c7-4460-a021-e07e4852d20e.mp4'
-						controls
-						loop
-						preload='auto'
-					>
-						Tu navegador no admite el elemento <code>video</code>.
-					</video>
-				</div>
+					<img
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg'
+					/>
+					<img
+						className='w-12'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg'
+					/>
+				</span>
 			</div>
 		</div>
 	)
